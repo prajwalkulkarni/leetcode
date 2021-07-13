d95f305ce385907ef6eeb281e76f138bb5bda32e
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-07-13 15:23:30 +0530

    Update and rename inorder_traversal.py to bst_traversal.py

--- bst_traversal.py
+++ bst_traversal.py
@@ -33,3 +33,42 @@ class Solution(object):
         
             self.recursive(root.right)
         
+"""
+Pre-oder traversal
+"""
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        
+        Solution.inorder=[]
+        
+        #inorder=[]
+        self.recursive(root)
+        
+        return Solution.inorder
+    
+    def recursive(self,root):
+        
+        if root is not None:
+            
+            
+            Solution.inorder.append(root.val)
+            self.recursive(root.left)
+        
+            
+        
+            self.recursive(root.right)
+        
+        
+        
+    
