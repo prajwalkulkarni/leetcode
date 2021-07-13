8adbf0491f5705ea4e6a1cf6311538bd32f7ff26
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-07-13 15:20:25 +0530

    Create inorder_traversal.py

---
+++ inorder_traversal.py
@@ -0,0 +1,35 @@
+"""
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+"""
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    inorder=[]
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        Solution.inorder=[]
+        
+        #inorder=[]
+        self.recursive(root)
+        
+        return Solution.inorder
+        
+        
+    def recursive(self,root):
+        
+        if root is not None:
+            
+            self.recursive(root.left)
+        
+            Solution.inorder.append(root.val)
+        
+            self.recursive(root.right)
+        
