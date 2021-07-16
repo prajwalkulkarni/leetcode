cf7cefbc1de40327b635a2ad748795b775c3d499
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-07-16 13:16:53 +0530

    Update longest_substr_palindrome.py

--- longest_substr_palindrome.py
+++ longest_substr_palindrome.py
@@ -10,3 +10,35 @@ Output: "bab"
 Note: "aba" is also a valid answer.
 """
 
+
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        string = list(s)
+        highest=['']
+        substr=[]
+        forward=[]
+        backward=[]
+        
+        if s[::] == s[::-1]:
+            return s
+        for i in range(0,len(s)):
+            k=0
+            
+            for j in range(i,len(s)):
+                
+                forward = str(s[i:len(s)-k])
+                backward = forward[::-1]
+                #backward+=s[k]
+                k+=1
+                
+                if forward == backward:
+                    
+                    highest.append(str(forward))
+        
+        return max(highest,key=len)
+                
+
