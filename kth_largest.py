782d5782c7e23247ac966b928d0b03766cddd6ea
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-07-17 13:22:22 +0530

    Update kth_largest.py

--- kth_largest.py
+++ kth_largest.py
@@ -4,3 +4,14 @@ Given an integer array nums and an integer k, return the kth largest element in 
 Note that it is the kth largest element in the sorted order, not the kth distinct element.
 """
 
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        
+        
+        
+        return sorted(nums)[len(nums)-1-(k-1)]
