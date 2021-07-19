add500b1f5e23236c1e8f5914970c420d51f34b1
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-07-19 16:16:26 +0530

    Update longest_common_prefix.py

--- longest_common_prefix.py
+++ longest_common_prefix.py
@@ -64,3 +64,67 @@ class Solution(object):
  """           
             
         
+ class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        
+        self.strs = strs
+        
+        """     
+        longest=''
+        
+        smallest=min(strs,key=len)
+        strs.remove(smallest)
+        
+        fstrs = ''.join(strs)
+        
+        flag=0
+        
+        
+        treshold = len(smallest)
+        j=0
+        while j<treshold:
+            #print(smallest[j:treshold])
+            if fstrs.count(smallest[j:treshold])==len(strs):
+                count=0
+                for word in strs:
+                    if word[0:len(smallest[j:treshold])] == smallest[j:treshold]:
+                        count+=1
+                if count == len(strs):
+                    flag=1
+                    longest = smallest[j:treshold]
+                    break
+
+            treshold-=1
+           
+                
+            
+        
+        return longest
+        """
+        
+        word = strs[0]
+        
+        for i in range(1,len(strs)):
+            treshold=len(word)
+            while word not in strs[i][0:treshold]:
+                treshold-=1
+                word = str(word[0:treshold])
+                
+            
+            
+            if not str(word)==str(strs[i][0:len(word)]):
+                word=""
+            
+        
+        if word=="":
+            return ""
+        return word
+                
+                
+            
+            
+        
