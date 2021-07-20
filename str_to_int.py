1b557c000fd842ed8d52f3601f2cacbb68630f33
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-07-20 17:56:45 +0530

    Create str_to_int.py

---
+++ str_to_int.py
@@ -0,0 +1,94 @@
+"""
+Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer (similar to C/C++'s atoi function).
+
+The algorithm for myAtoi(string s) is as follows:
+
+Read in and ignore any leading whitespace.
+Check if the next character (if not already at the end of the string) is '-' or '+'. Read this character in if it is either. This determines if the final result is negative or positive respectively. Assume the result is positive if neither is present.
+Read in next the characters until the next non-digit charcter or the end of the input is reached. The rest of the string is ignored.
+Convert these digits into an integer (i.e. "123" -> 123, "0032" -> 32). If no digits were read, then the integer is 0. Change the sign as necessary (from step 2).
+If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then clamp the integer so that it remains in the range. Specifically, integers less than -231 should be clamped to -231, and integers greater than 231 - 1 should be clamped to 231 - 1.
+Return the integer as the final result.
+Note:
+
+Only the space character ' ' is considered a whitespace character.
+Do not ignore any characters other than the leading whitespace or the rest of the string after the digits.
+ 
+
+Example 1:
+
+Input: s = "42"
+Output: 42
+Explanation: The underlined characters are what is read in, the caret is the current reader position.
+Step 1: "42" (no characters read because there is no leading whitespace)
+         ^
+Step 2: "42" (no characters read because there is neither a '-' nor '+')
+         ^
+Step 3: "42" ("42" is read in)
+           ^
+The parsed integer is 42.
+Since 42 is in the range [-231, 231 - 1], the final result is 42.
+"""
+
+#Test cases passed: 771/1082
+
+import math
+class Solution(object):
+    def myAtoi(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        flag=0
+        numbers=['0','1','2','3','4','5','6','7','8','9']
+        number = ''
+        minimum = int(math.pow(-2,31))
+        maximum  = int(math.pow(2,31)-1)
+        s = str(s.strip())
+        
+        
+        
+        try:
+            number = int(s)
+            print(number)
+            if number < minimum:
+                return minimum
+            elif number > maximum:
+                return maximum
+            else:
+                return number
+        except ValueError:
+            
+            for index,c in enumerate(s):
+                if index==0 and c=="-":
+                    print("run")
+                    flag=1
+                    
+                else:
+                    if c in numbers:
+                        ("runrun")
+                        number+=c
+                    else:
+                        break
+            
+            print(number)
+            #return int(number)
+            if number =='':
+                return 0
+            elif flag==1:
+                number = int(number)
+                if number < minimum:
+                    return minimum
+                else:
+                    return -number
+            else:
+                number = int(number)
+                if number>maximum:
+                    return maximum
+                else:
+                    return number
+        
+            
+        
+                
+        
