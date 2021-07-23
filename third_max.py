404f4bc958ba6c0e8e08d28c916a84f7c5617fe5
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-07-23 12:37:14 +0530

    Create third_max.py

---
+++ third_max.py
@@ -0,0 +1,24 @@
+"""
+Given integer array nums, return the third maximum number in this array. If the third maximum does not exist, return the maximum number.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,1]
+Output: 1
+Explanation: The third maximum is 1.
+"""
+class Solution(object):
+    def thirdMax(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums = list(set(nums))
+        if len(nums) < 3:
+            return max(nums)
+        else:
+            for i in range(0,2):
+                nums.remove(max(nums))
+        return max(nums)
