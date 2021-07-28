9f2aa1af18ef5e48269bd9bf5d349a4505fb4a75
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-07-28 17:25:13 +0530

    add deletion

--- bst_bfs.py
+++ bst_bfs.py
@@ -84,3 +84,154 @@ while i<int(n):
 
 levelOrder(head)
 #inOrder(head)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Node:
+    
+    def __init__(self,left,right,val):
+        self.left = left
+        self.right = right
+        self.val=val
+    
+
+def createNode(val):
+    
+    node = Node(None,None,val)
+    
+    return node 
+
+
+
+def insert(root,key):
+    
+    if root is None:
+        return createNode(key)
+    
+    elif key>root.val:
+        root.right = insert(root.right,key)
+    else:
+        root.left = insert(root.left,key)
+    
+    return root
+
+def getHeight(root):
+    temp = root
+    rtemp = root
+    i=0
+    j=0
+    
+    while temp is not None:
+        i+=1
+        temp=temp.left
+    
+    while rtemp is not None:
+        j+=1
+        rtemp = rtemp.right
+    
+    if i>j:
+        return i
+    elif j>i:
+        return j
+    else:
+        return i
+
+
+def currentOrder(root):
+    h = getHeight(root)
+    #print(h)
+    
+    for j in range(1,h+1):
+        printCurrentLevelOrder(root,j)
+
+def printCurrentLevelOrder(root,level):
+    
+    if root is None:
+        return None
+    if level==1:
+        print(root.val)
+    if level>1:
+        printCurrentLevelOrder(root.left,level-1)
+        printCurrentLevelOrder(root.right,level-1)
+
+
+def inorderSuccessor(root):
+    
+    current = root
+    while current.left is not None:
+        current = current.left
+    
+    return current
+    
+def deleteNode(root,key):
+    
+    if root is None:
+        return root
+    
+    if key>root.val:
+        root.right = deleteNode(root.right,key)
+    elif key<root.val:
+        root.left = deleteNode(root.left,key)
+    
+    else:
+        
+        if root.left is None:
+            temp = root.right
+            root = None
+            return temp
+        
+        elif root.right is None:
+            temp = root.left
+            root = None
+            return temp
+        
+        #two children
+        
+        temp = inorderSuccessor(root.right)
+        print(temp,temp.val)
+        root.val = temp.val
+        
+        root.right = deleteNode(root.right,temp.val)
+    return root
+    
+n = input("How many elements")
+
+i=0
+root = None
+while i<int(n):
+    
+    data = input("Enter number")
+    root = insert(root,int(data))
+    i+=1
+
+currentOrder(root)   
+
+
+rm = input("Enter node to be deleted")
+
+deleteNode(root,int(rm))
+
+currentOrder(root)
+        
