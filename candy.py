777e0d80cf14b17766a619b4e7945ef21039f886
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-07-29 14:36:23 +0530

    Update candy.py

--- candy.py
+++ candy.py
@@ -21,3 +21,43 @@ Output: 4
 Explanation: You can allocate to the first, second and third child with 1, 2, 1 candies respectively.
 The third child gets 1 candy because it satisfies the above two conditions.
 """
+
+
+class Solution(object):
+    def candy(self, ratings):
+        """
+        :type ratings: List[int]
+        :rtype: int
+        """
+       
+    
+        if len(ratings) == 0:
+            return 0
+        elif list(set(ratings))==1:
+            return len(ratings)
+        
+        
+        maximum = max(ratings)
+        maxNo = len(ratings)
+        minimum = [1 for i in range(len(ratings))]
+        
+        
+        for i in range(1,len(ratings)):
+            if ratings[i]>ratings[i-1]:
+                minimum[i]+=minimum[i-1]
+            elif ratings[i-1]>ratings[i]:
+                j=i-1
+                k=0
+                
+                while j+1<=len(ratings)-1 and ratings[j]>ratings[j+1] :
+                    #print("one")
+                    k+=1
+                    j+=1
+                
+                
+                    
+                if not minimum[i-1]>k:
+                    minimum[i-1]=k+1
+            
+        
+        return sum(minimum)
