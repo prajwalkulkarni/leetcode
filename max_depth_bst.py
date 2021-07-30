7eb34e61a9c3490de87d20b0525a1f582532c4b9
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-07-30 15:59:26 +0530

    Update max_depth_bst.py

--- max_depth_bst.py
+++ max_depth_bst.py
@@ -5,3 +5,27 @@ A binary tree's maximum depth is the number of nodes along the longest path from
 
  
 """
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        
+        if root is None:
+            return 0
+        
+        ldepth = self.maxDepth(root.left)
+        rdepth = self.maxDepth(root.right)
+        
+        if ldepth>rdepth:
+            return ldepth+1
+        else:
+            return rdepth+1
