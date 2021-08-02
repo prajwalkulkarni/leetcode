25902e3d1d0e392a4b42e0772b25af408f7f771f
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-08-02 13:26:53 +0530

    Update invert_binary_tree.py

--- invert_binary_tree.py
+++ invert_binary_tree.py
@@ -10,3 +10,26 @@ Input: root = [4,2,7,1,3,6,9]
 Output: [4,7,2,9,6,3,1]
 """
 
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        
+        if not root:
+            return None
+        
+        rnode = self.invertTree(root.right)
+        lnode = self.invertTree(root.left)
+        
+        
+        root.left,root.right = rnode,lnode
+        
+        return root
