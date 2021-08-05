09d5a125fd4d8ff49ae05f668ae0d37021704390
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-08-05 15:45:21 +0530

    Update 3Sum.py

--- 3Sum.py
+++ 3Sum.py
@@ -18,3 +18,39 @@ Example 3:
 Input: nums = [0]
 Output: []
 """
+
+
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        
+        n = len(nums)
+        
+        if n<=2:
+            return []
+        #nums = sorted(nums)
+        res=[]
+        for i in range(0,n-2):
+            for j in range(i+1,n-1):
+                """
+                for k in range(j+1,n):
+                    if nums[i]+nums[j]+nums[k]==0 and sorted([nums[i],nums[j],nums[k]]) not in res:
+                        
+                        res.append(sorted([nums[i],nums[j],nums[k]]))
+                """
+                cumsum = nums[i]+nums[j]
+                opp = -cumsum
+                
+                if opp not in nums:
+                    break
+                else:
+                    threeSum = sorted([nums[i],nums[j],opp])
+                    if threeSum not in res:
+                        res.append(threeSum)
+                        break
+        
+        return res
+                    
