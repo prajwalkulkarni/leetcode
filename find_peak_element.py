559628037f01ec5d264f4d20564b08d42afcf9ed
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-08-06 15:21:20 +0530

    Update find_peak_element.py

--- find_peak_element.py
+++ find_peak_element.py
@@ -18,3 +18,42 @@ Input: nums = [1,2,1,3,5,6,4]
 Output: 5
 Explanation: Your function can return either index number 1 where the peak element is 2, or index number 5 where the peak element is 6.
 """
+
+
+#Can use binary search or regular for loop to find peak element in an unsorted array
+
+class Solution(object):
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        """
+        j=0
+        for i in range(1,len(nums)-1):
+            #print(i)
+            if nums[i]>nums[i-1] and nums[i]>nums[i+1]:
+                j=i
+                break
+        
+        #return j
+        if j==0:
+            return nums.index(max(nums))
+        else:
+            return j
+        """
+        ans=-1
+        low = 0
+        high = len(nums)
+        
+        while low<=high:
+            m = (low+high)//2
+            
+            if nums[m]>nums[m-1] and nums[m] >nums[m+1]:
+                ans=m
+                break
+            elif nums[m]>nums[m-1] and nums[m]<nums[m+1]:
+                high = m-1
+            elif nums[m]>nums[m+1] and nums[m]<nums[m-1]:
+                low = m+1
+        return ans
