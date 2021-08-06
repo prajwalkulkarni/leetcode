8aa0ff2fc2c6d1fa27456ad2dff7c5ad28c283d9
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-08-06 19:12:40 +0530

    Update jump_game.py

--- jump_game.py
+++ jump_game.py
@@ -14,3 +14,49 @@ Input: nums = [3,2,1,0,4]
 Output: false
 Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
 """
+
+
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        
+        di = len(nums) - 1
+        
+        for i in range(len(nums)-1,-1,-1):
+            if nums[i]>=di-i:
+                di = i
+                
+        
+        return di==0
+    """
+    def jumpGame(self,nums,pos,visited):
+        
+        #toPos = skip
+        if pos==len(nums)-1:
+            return True
+        
+        if nums[pos]==0:
+            return False
+        
+        if pos>len(nums)-1:
+            return False
+        
+        if pos in visited:
+            return False
+        length = nums[pos]
+        
+        for i in range(1,length+1):
+            if self.jumpGame(nums,pos+i,visited):
+                return True
+            
+             
+        
+        visited.append(pos)
+        return False
+        #return self.jumpGame(nums,pos+1)
+        #return False
+    """
+        
