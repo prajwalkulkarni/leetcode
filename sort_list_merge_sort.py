ee918b222c6823034126856e962cad108deec3b5
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-08-08 09:46:24 +0530

    Update sort_list_merge_sort.py

--- sort_list_merge_sort.py
+++ sort_list_merge_sort.py
@@ -12,3 +12,83 @@ Input: head = [4,2,1,3]
 Output: [1,2,3,4]
 """
 
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    #sortedList=[]
+    def sortList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        i=0
+        temp = head
+        while temp is not None:
+            i+=1
+            temp=temp.next
+        
+        sortedList = [0 for j in range(i)]
+        
+        temp = head
+        i=0
+        while temp is not None:
+            sortedList[i] = temp.val
+            temp = temp.next
+            i+=1
+        
+        #print(sortedList)
+        
+        sortedList = self.mergeSort(sortedList)
+        
+        i=0
+        temp = head
+        while temp is not None:
+            temp.val = sortedList[i]
+            temp = temp.next
+            i+=1
+        
+        return head
+    
+    def mergeSort(self,nums):
+        
+        l = 0
+        r = len(nums)
+        if len(nums)>1:
+            
+            
+            
+            mid = (l+r)//2
+            
+            leftArr = nums[:mid]
+            rightArr = nums[mid:]
+            
+            self.mergeSort(leftArr)
+            self.mergeSort(rightArr)
+            
+            i=j=k=0
+            
+            while i<len(leftArr) and j<len(rightArr):
+                if leftArr[i]<rightArr[j]:
+                    nums[k] = leftArr[i]
+                    i+=1
+                    k+=1
+                else:
+                    nums[k] = rightArr[j]
+                    j+=1
+                    k+=1
+            
+            while i<len(leftArr):
+                nums[k]=leftArr[i]
+                i+=1
+                k+=1
+            
+            while j<len(rightArr):
+                nums[k] = rightArr[j]
+                j+=1
+                k+=1
+        return nums
+        
+        
