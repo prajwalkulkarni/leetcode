b62fd96908795005e7d9f5dedf6c06b92825079c
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-08-08 09:45:14 +0530

    Create sort_list_merge_sort.py

---
+++ sort_list_merge_sort.py
@@ -0,0 +1,14 @@
+"""
+Given the head of a linked list, return the list after sorting it in ascending order.
+
+Follow up: Can you sort the linked list in O(n logn) time and O(1) memory (i.e. constant space)?
+
+ 
+
+Example 1:
+
+
+Input: head = [4,2,1,3]
+Output: [1,2,3,4]
+"""
+
