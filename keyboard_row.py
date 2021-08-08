748366fd495872e3d6e538471d598bb153be60d8
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-08-08 15:43:50 +0530

    Update keyboard_row.py

--- keyboard_row.py
+++ keyboard_row.py
@@ -9,3 +9,48 @@ the third row consists of the characters "zxcvbnm".
 
 """
 
+class Solution(object):
+    def findWords(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        
+        can=[]
+        n=len(words)
+        
+        #words = [i.lower() for i in words]
+        keyboard = ["QWERTYUIOPqwertyuiop","ASDFGHJKLasdfghjkl","ZXCVBNMzxcvbnm"]
+        
+        toCheck =  -1
+        string=""
+        flag=0
+        for i in range(0,n):
+            #print(words[i][0])
+            if words[i][0] in keyboard[0]:
+                toCheck = 0
+            elif words[i][0] in keyboard[1]:
+                #print("H here;")
+                toCheck = 1
+            elif words[i][0] in keyboard[2]:
+                toCheck = 2
+            
+            
+            if toCheck != -1:
+                #print("XX")
+                for j in words[i]:
+                    if j in keyboard[toCheck]:
+                        string+=j
+                    else:
+                        string=""
+                        break
+                        
+                if string == words[i]:
+                    #print("Yes")
+                    can.append(words[i])
+                    string=""
+            
+        
+        
+        return can
+                
