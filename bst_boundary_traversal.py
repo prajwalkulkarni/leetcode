1c6df22506214ba35c0031b099648df18db4f5bc
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-08-12 12:08:35 +0530

    Update bst_boundary_traversal.py

--- bst_boundary_traversal.py
+++ bst_boundary_traversal.py
@@ -1,3 +1,47 @@
 """
 Traverse boundary nodes of a bst.
 """
+
+
+def traverseLeaves(root):
+  
+  if root:
+    traverseLeaves(root.left)
+    
+    if root.left is None and root.right is None:
+      print(root.data)
+    traverseLeaves(root.right)
+
+    
+def boundaryLeft(root):
+  if root:
+    
+    if root.left:
+      print(root.data)
+      boundaryLeft(root.left)
+    
+    elif root.right:
+      print(root.data)
+      boundaryLeft(root.right)
+
+def boundaryRight(root):
+  if root:
+    
+    if root.right:
+      print(root.data)
+      boundaryRight(root.right)
+     
+    elif root.left:
+      print(root.data)
+      boundaryRight(root.left)
+
+def boundaryTraversal(root):
+  
+  if root:
+    print(root.data)
+    boundaryLeft(root)
+    
+    traverseLeaves(root)
+    
+    boundaryRight(root)
+ 
