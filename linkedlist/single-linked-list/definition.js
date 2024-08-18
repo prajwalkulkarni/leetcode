72dfb9dff61b8ad5a448f75001b25f2c21741020
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2024-08-18 18:02:17 +0530

    Update definition.js

--- linkedlist/single-linked-list/definition.js
+++ linkedlist/single-linked-list/definition.js
@@ -22,3 +22,67 @@ function arrayToLinkedList(arr){
 
   return head;
 }
+
+
+
+// Insert new head to LL
+
+
+
+const currHead = arrayToLinkedList(arr)
+
+const newHead = new Node(0);
+newHead.next = currHead;
+
+
+//Delete tail in a LL
+
+function deleteTail(arr){
+
+  const head = arrayToLinkedList(arr);
+
+  let prev = head;
+  let curr = head;
+
+  while(curr.next !== null){
+    prev = curr;
+    curr = curr.next
+  }
+
+  prev.next = null;
+  
+}
+
+//Get length of LL
+
+function getLength(head){
+
+  let count = 0;
+  let curr = head;
+
+  while(curr.next !== null){
+    count+=1;
+    curr = curr.next
+  }
+
+  return count + 1;
+}
+
+
+//Search an element in a linked list
+
+function searchElement(head, item){
+
+  let temp = head;
+
+  while(temp.next){
+    if(temp.data === item){
+      return 1;
+    }
+
+    temp = temp.next;
+  
+  }
+
+  return 0;
+}
