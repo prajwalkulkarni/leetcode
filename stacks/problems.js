c8d6c235692373faca7222e03d4bae0e3396868f
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2024-09-04 20:46:55 +0530

    Create problems.js

---
+++ stacks/problems.js
@@ -0,0 +1,13 @@
+// Next greater element
+/**
+Problem Statement: Given a circular integer array A, return the next greater element for every element in A. The next greater element for an element x is the first element greater than x that we come across while traversing the array in a clockwise manner. If it doesn't exist, return -1 for this element.
+
+Examples:
+
+Example 1: 
+
+Input: N = 11, A[] = {3,10,4,2,1,2,6,1,7,2,9}
+
+Output: 10,-1,6,6,2,6,7,7,9,9,10
+
+*/
