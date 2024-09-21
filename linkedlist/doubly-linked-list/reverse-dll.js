10d198fbab209aef99c6deaf787188b198c6c6da
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2024-09-21 20:42:40 +0530

    Update reverse-dll.js

--- linkedlist/doubly-linked-list/reverse-dll.js
+++ linkedlist/doubly-linked-list/reverse-dll.js
@@ -13,34 +13,32 @@ class Node{
 
 function createDLL(){
   let arr= [1,2,3,4,5,6,7,8,9];
-
-const head = new Node(arr[0])
-let curr = head;
-for(let i = 1;i<arr.length; ++i){
-  const node = new Node(arr[i]);
-  node.prev = curr;
-  curr.next = node;
-  curr = node;
+  const head = new Node(arr[0])
+  let curr = head;
+  for(let i = 1;i<arr.length; ++i){
+    const node = new Node(arr[i]);
+    node.prev = curr;
+    curr.next = node;
+    curr = node;
+  }
+  return head;
 }
 
-curr = head;
-
-while(curr.next !== null){
-  console.log(curr.val);
-  curr = curr.next;
-}
-return head;
-}
-
-function reverse(head){
+function reverse(current){
   let temp = null;
-  while (current != null) {
+  while (current !== null) {
             temp = current.prev;
             current.prev = current.next;
             current.next = temp;
             current = current.prev;
   }
-  console.log(curr);
-
-  
+  console.log(current);
 }
+
+const head = createDLL();
+let curr = head;
+while(curr.next !== null){
+  curr = curr.next;
+}
+
+reverse(curr)
